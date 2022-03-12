079ed7058f78713f75d4f7b278d46144532074a3
Michael Davichick <mdavichick@mail.sfsu.edu>
2022-03-12 12:15:57 -0800

    Finished FrontEnd with states, proceeding with backend

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,30 @@ function App() {
   const [id, setID] = useState(0);
   const [password, setPassword] = useState("");
 
+  const displayInfo = () => {
+    console.log(name + pnumber + email + id + password);
+  };
+  
   return (
     <div className="App">
       <div className="information">
-      <h1>Create User</h1>
-      <label>Name</label>
-      <input type="text" />
-      <label>Phone Number</label>
-      <input type="text" />
-      <label>Email</label>
-      <input type="email" />
-      <label>Student ID</label>
-      <input type="number" />
-      <label>Password</label>
-      <input type="number" />
-      <button>Add User</button>
+        <h1>Create User</h1>
+          <label>Name</label>
+          <input type="text" 
+            onChange={(event) => {setName(event.target.value);}}/>
+          <label>Phone Number</label>
+          <input type="text" 
+            onChange={(event) => {setPhone(event.target.value);}}/>
+          <label>Email</label>
+          <input type="email" 
+            onChange={(event) => {setEmail(event.target.value);}}/>
+          <label>Student ID</label>
+          <input type="number" 
+            onChange={(event) => {setID(event.target.value);}}/>
+          <label>Password</label>
+          <input type="text"
+            onChange={(event) => {setPassword(event.target.value);}}/>
+          <button onClick={displayInfo}>Add User</button>
       </div>
     </div>
   );
