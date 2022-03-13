1c6bccfb7454c9926c4d939955613bcbc1019799
Michael Davichick <mdavichick@mail.sfsu.edu>
2022-03-13 09:57:57 -0700

    Migrated App.js to it's own page, implemented routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,17 @@
 import './App.css';
-import {useState} from "react";
-import Axios from "axios";
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import CreateUser from './CreateUser';
+import Results from './Results';
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [userid, setID] = useState(0);
-  const [password, setPassword] = useState("");
-
-  const displayInfo = () => {
-    console.log(name + phone + email + userid + password);
-  };
-  
-
-  const addUser = () => {
-    Axios.post('http://localhost:3001/create', {
-      userid: userid, 
-      name: name, 
-      phone: phone, 
-      email: email, 
-      password: password
-    }).then(() => {
-      console.log("success");
-    });
-  };
-
   return (
-    <div className="App">
-      <div className="information">
-        <h1>Create User</h1>
-          <label>Name</label>
-          <input type="text" 
-            onChange={(event) => {setName(event.target.value);}}/>
-          <label>Phone Number</label>
-          <input type="text" 
-            onChange={(event) => {setPhone(event.target.value);}}/>
-          <label>Email</label>
-          <input type="email" 
-            onChange={(event) => {setEmail(event.target.value);}}/>
-          <label>Student ID</label>
-          <input type="number" 
-            onChange={(event) => {setID(event.target.value);}}/>
-          <label>Password</label>
-          <input type="text"
-            onChange={(event) => {setPassword(event.target.value);}}/>
-          <button onClick={addUser}>Add User</button>
-      </div>
-    </div>
+   <Router>
+     <Routes>
+       <Route path="/" exact element ={<CreateUser/>} />
+       <Route path="/results" exact element ={<Results/>} />
+     </Routes>
+   </Router>
   );
 }
 
---
+++ client/src/Results.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useState} from "react";
+import Axios from "axios";
+
+function Results() {
+    return (
+     <div>
+         <h1>Testing</h1>
+     </div>
+    );
+  }
+
+  export default Results;
